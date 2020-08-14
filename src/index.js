233f72e199f6c11a32f08b727e64eddf0e537249
Hoang Anh Kiet <hoanganhkiet.div@gmail.com>
2020-08-14 18:37:11 +0700

    Update youtube route handler

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import express from 'express';
 import apiRouter from './routers/api';
 
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send(req.originalUrl);
 });
 
-app.get('/api', apiRouter);
+app.use('/api', apiRouter);
 
-app.use((err, res, res, next) => {
+app.use((err, req, res, next) => {
     console.log(err.stack);
     res.status(500).send('Something broke!');
 });
 
 app.use((req, res, next) => {
     res.status(404).send("Sorry I can't find that");
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
--- src/routers/api.js
+++ src/routers/api.js
@@ -1,9 +1,47 @@
-import { Router } from "express";
+import express, { json } from "express";
+import axios from "axios";
 
-const apiRouter = Router();
+const apiRouter = express.Router();
 
 apiRouter.get('/youtube/:videoId', (req, res) => {
-
+    const videoId = req.params.videoId;
+    const headers = {
+        'X-YouTube-Client-Name': '1',
+        'X-YouTube-Client-Version': '2.20200806.01.01',
+        'X-SPF-Referer': 'https://www.youtube.com/',
+        'X-SPF-Previous': 'https://www.youtube.com/'
+    };
+    axios.get('https://www.youtube.com/watch', {
+        params: {
+            v: videoId,
+            pbj: 1
+        },
+        headers: headers
+    })
+        .then(response => {
+            try {
+                let status = response.data?.[3]?.playerResponse?.playabilityStatus?.status;
+                if (status && status === 'OK') {
+                    let data = response.data?.[2]?.player?.args?.player_response;
+                    data = JSON.parse(data);
+                    if (data && data?.streamingData?.formats) {
+                        let responseData = data?.streamingData?.formats;
+                        res.json({
+                            status: 'OK',
+                            data: responseData
+                        });
+                        return;
+                    }
+                }
+                throw Error('something is wrong');
+            }
+            catch (err) {
+                res.json({ status: 'error', message: err });
+            }
+        })
+        .catch(err => {
+            res.json({ status: 'error', message: err });
+        });
 });
 
 export default apiRouter;
